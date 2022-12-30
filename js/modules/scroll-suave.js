83a7fb87c53615ab5d0bde2f7e0b0233185cf602
Claudionilton <claudioniltonjose@gmail.com>
2022-12-30 18:23:35 -0300

    Refatorando funcionalidades do site

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -2,10 +2,7 @@ export default class ScrollSuave {
   constructor(links, options) {
     this.linksInternos = document.querySelectorAll(links);
     if (options === undefined) {
-      this.options = {
-        behavior: 'smooth',
-        block: 'start',
-      };
+      this.options = { behavior: 'smooth', block: 'start' };
     } else {
       this.options = options;
     }
